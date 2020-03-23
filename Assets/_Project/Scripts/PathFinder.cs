c4c39adace1d27c8df3ac5dd7331ef77be72cb76
guraysenova <guraysenova@outlook.com>
2020-03-23 10:58:31 +0300

    2019.3.6 update

--- Assets/_Project/Scripts/PathFinder.cs
+++ Assets/_Project/Scripts/PathFinder.cs
@@ -20,15 +20,7 @@ public class PathFinder : MonoBehaviour
     public void SetGrid(List<TileData> tileData , TwoDCoordinate boardSize)
     {
         grid = new bool[boardSize.x, boardSize.y];
-        int index = 0;
-        for (int y = 0; y < boardSize.y; y++)
-        {
-            for (int x = 0; x < boardSize.x; x++)
-            {
-                grid[x, y] = tileData[index].isFilled;
-                index++;
-            }
-        }
+        UpdateGrid(tileData, boardSize);
     }
 
     public void UpdateGrid(List<TileData> tileData, TwoDCoordinate boardSize)
@@ -42,5 +34,19 @@ public class PathFinder : MonoBehaviour
                 index++;
             }
         }
+    }
+
+    public List<TwoDCoordinate> GetPath(TwoDCoordinate agentPos , TwoDCoordinate targetPos)
+    {
+        TwoDCoordinate pos = new TwoDCoordinate();
+        pos.x = agentPos.x / 2;
+        pos.y = agentPos.y / 2;
+        TwoDCoordinate target = new TwoDCoordinate();
+        target.x = targetPos.x / 2;
+        target.y = targetPos.y / 2;
+
+
+
+        return null;
     }
 }
--- Assets/_Project/Scripts/Unfold.cs
+++ Assets/_Project/Scripts/Unfold.cs
@@ -1,20 +1,9 @@
 ﻿using UnityEngine;
-
 public class Unfold : MonoBehaviour
 {
-    bool unFolded;
-
-    private void Awake()
+    void Awake()
     {
         TriggerAnimator("Unfold");
-    }
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0) && !unFolded)
-        {
-            //TriggerAnimator("Unfold");
-            unFolded = true;
-        }
     }
     void TriggerAnimator(string triggerString)
     {
