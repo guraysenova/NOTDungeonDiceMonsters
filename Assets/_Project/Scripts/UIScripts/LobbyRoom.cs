5943c6c613522d5a9f216aca9a9bb02336b514cf
M. Guray Senova <guraysenova@outlook.com>
2020-08-01 02:06:32 +0300

    up
    up

---
+++ Assets/_Project/Scripts/UIScripts/LobbyRoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LobbyRoom : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI roomName, playerCount, gameType;
+
+    string UUID;
+
+    public void SetData(string roomNameText , int playerAmount , string gameTypeText , string UUID)
+    {
+        roomName.text = roomNameText;
+        playerCount.text = playerAmount + "/2";
+        gameType.text = gameTypeText;
+        this.UUID = UUID;
+    }
+
+    public void JoinRoom()
+    {
+
+    }
+}
